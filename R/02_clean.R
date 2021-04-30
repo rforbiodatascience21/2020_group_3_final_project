521a21cdc69fa0b5a5d49061bc9466bbf4e1a233
AugustEhl <august.ehlers@gmail.com>
2021-04-30 08:59:30 +0000

    cleaned Age and HbA1c columns and rounded BMI

--- R/02_clean.R
+++ R/02_clean.R
@@ -7,15 +7,19 @@ library("tidyverse")
 
 
 # Define functions --------------------------------------------------------
-source(file = "R/99_project_functions.R")
-
-
-# Load data ---------------------------------------------------------------
-my_data <- read_tsv(file = "data/01_my_data.tsv")
-
+source(file = "R/01_load.R")
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean <- my_data # %>% ...
+my_data_clean <- my_data %>%
+  mutate(Age = case_when(Age == "greater then 15" ~ "> 15",
+                         Age == "Less then 11" ~ "< 11",
+                         Age == "Less then 15" ~"< 15",
+                         Age == "Less then 5" ~ "< 5"),
+         HbA1c = case_when(HbA1c == "Over 7.5%" ~"> 7.5%",
+                           HbA1c == "Less then 7.5%" ~"< 7.5%"),
+         BMI = round(BMI,1))
+
+
 
 
 # Write data --------------------------------------------------------------
