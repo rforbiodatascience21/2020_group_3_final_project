62c4fb0c2d7c995818cc715fcd20f8a83817f242
AugustEhl <august.ehlers@gmail.com>
2021-04-30 10:02:15 +0000

    Changed small error

--- R/02_clean.R
+++ R/02_clean.R
@@ -20,14 +20,22 @@ my_data_clean <- my_data %>%
          HbA1c = case_when(HbA1c == "Over 7.5%" ~"> 7.5%",
                            HbA1c == "Less then 7.5%" ~"< 7.5%"),
          BMI = round(BMI,1),
-         Dur_disease = str_extract(`Duration of disease`,"\\d+\\.?\\d*"),
-         unit = str_replace(`Duration of disease`, Dur_disease,"")) %>%
+         Dur_disease(days) = str_extract(`Duration of disease`,"\\d+\\.?\\d*"),
+         unit = str_replace(`Duration of disease`, Dur_disease(days),"")) %>%
   select(-`Duration of disease`)
 
+# Converting duration to year for every value
+my_data_clean <- my_data_clean %>%
+  mutate(Dur_disease(days) = as.numeric(Dur_disease(days))) %>%
+  mutate(Dur_disease(days) = case_when(unit == "d" ~ Dur_disease(days),
+                                 unit == "w" ~ Dur_disease(days) * 7,
+                                 unit == "m" ~ Dur_disease(days) * 30,
+                                 unit == "y" ~ Dur_disease(days) * 365)) %>%
+  select(-unit)
 
 
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data_clean,
-          file = "data/02_my_data_clean.tsv")
+#write_tsv(x = my_data_clean,
+#          file = "data/02_my_data_clean.tsv")
