c7710c8d4630916c824e872b4a06a36127e262ec
HCEhlers <holgerehlers@hotmail.com>
2021-05-02 20:01:27 +0000

    Most of PCA done

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -8,6 +8,8 @@ library(devtools)
 library(dplyr)
 library(tidymodels)
 library(patchwork)
+library(broom)
+library(cowplot)
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
@@ -25,15 +27,7 @@ my_data_clean_aug %>%
   count(my_data_clean_aug)
 
 
-# Model data
-x <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
-View(x)
-numeric_data <- my_data_clean_aug %>% select(BMI, Weight,Height, Dur_disease) %>%
-  drop_na()
-
-dim(numeric_data)
-View(my_data_clean_aug)
-# Data exploration
+# Model data and Data exploration
 
 # We plot a bar chart of duration of type 1 diabetes and
 
@@ -75,14 +69,69 @@ ggplot(data = my_data_clean_aug,
 
 # PCA
 
-diabetes_1_pca <- my_data_clean_aug %>%
-  drop_na() %>%
-  nest() %>% 
-  mutate(pca = map(data, ~ prcomp(.x %>% select(where(is.numeric)), 
-                                  center = TRUE, scale = TRUE)),
-         pca_aug = map2(pca, data, ~augment(.x, data = .y)))
+# Removing NA's
+my_data_clean_aug_dop_na <- my_data_clean_aug %>% drop_na()
 
-diabetes_1_pca
+pca_fit <- my_data_clean_aug_dop_na %>%
+  select(where(is.numeric)) %>% # retain only numeric columns
+  prcomp(scale = TRUE)
+
+pca_fit %>% 
+  augment(my_data_clean_aug_dop_na) %>%
+  ggplot(aes(.fittedPC1, .fittedPC2, color = Hypoglycemis)) + 
+  geom_point(size = 1.5) +
+  scale_color_manual(
+    values = c(Yes = "#D55E00", No = "#0072B2")
+  ) +
+  theme_half_open(12) + background_grid()
+
+# Looking hard to distinguish based on PC1 and PC2
+
+# Extracting rotation matrix
+pca_fit %>%
+  tidy(matrix = "rotation")
+
+
+# define arrow style for plotting
+arrow_style <- arrow(
+  angle = 20, ends = "first", type = "closed", length = grid::unit(8, "pt")
+)
+
+# Plotting rotation matrix
+pca_fit %>%
+  tidy(matrix = "rotation") %>%
+  pivot_wider(names_from = "PC",
+              names_prefix = "PC",
+              values_from = "value") %>%
+  ggplot(aes(PC1, PC2)) +
+  geom_segment(xend = 0, yend = 0, arrow = arrow_style) +
+  geom_text(
+    aes(label = column),
+    hjust = 1, nudge_x = -0.02, 
+    color = "#904C2F"
+  ) +
+  xlim(-1.25, .5) + ylim(-.5, 1) +
+  coord_fixed() + # fix aspect ratio to 1:1
+  theme_minimal_grid(12)
+
+# Abbove not working atm
+
+# This works fine
+pca_fit %>%
+  tidy(matrix = "eigenvalues")
+
+pca_fit %>%
+  tidy(matrix = "eigenvalues") %>%
+  ggplot(aes(PC, percent)) +
+  geom_col(fill = "#56B4E9", alpha = 0.8) +
+  scale_x_continuous(breaks = 1:9) +
+  scale_y_continuous(
+    labels = scales::percent_format(),
+    expand = expansion(mult = c(0, 0.01))
+  ) +
+  theme_minimal_hgrid(12)
+
+
 # Visualise data ----------------------------------------------------------
 my_data_clean_aug %>% ggplot(
   aes(x = BMI,
