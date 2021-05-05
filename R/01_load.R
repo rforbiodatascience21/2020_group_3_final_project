12dbd6a5f158e665cb24d1787c5e6769c2c2885b
AugustEhl <august.ehlers@gmail.com>
2021-05-05 13:35:13 +0000

    removed empty line

--- R/01_load.R
+++ R/01_load.R
@@ -5,7 +5,6 @@ rm(list = ls())
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 
-
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
