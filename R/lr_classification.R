8b053b80cec49179c7f356ac4d6887ab2000a9b6
HCEhlers <holgerehlers@hotmail.com>
2021-05-07 08:02:34 +0000

    updates to model

--- R/lr_classification.R
+++ R/lr_classification.R
@@ -7,35 +7,50 @@ library(tidyverse)
 library(workflows)
 library(tune)
 library(ranger)
-
+library(dplyr)
+library(purrr)
+library(broom)
 
 # Load data ---------------------------------------------------------------
 my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
-View(my_data_clean_aug)
 # Data wrangling ----------------------------------------------------------
 lr_fit <- my_data_clean_aug %>%
-  mutate(Affected = case_when(Affected == 0 ~ "No",
-                              Affected == 1 ~ "yes"))
-View(my_data_clean_aug)
+  mutate(Affected = factor(case_when(Affected == 0 ~ "No",
+                                     Affected == 1 ~ "yes")))
+
 
 
 # Model data --------------------------------------------------------------
+lr_fit <- lr_fit %>%
+  mutate(mdl = map(data, ~ glm(formula = Affected ~ Height + Weight + BMI + Dur_disease,
+                               data = .,
+                               family = binomial(link = "logit"))))
+
+log_mod <- logistic_reg() %>%
+  set_engine("glm")
+
+log_fit <- log_mod %>%
+  fit(Affected ~ Height + Weight + BMI + Dur_disease + genderBin,
+      data = lr_fit)
+log_fit
+
 
 set.seed(234589)
 # split the data into trainng (75%) and testing (25%)
 t1_diabetes_split <- initial_split(lr_fit, 
                                 prop = 3/4)
 
-diabetes_train <- training(diabetes_split)
-diabetes_test <- testing(diabetes_split)
+diabetes_train <- training(t1_diabetes_split)
+diabetes_test <- testing(t1_diabetes_split)
 
 # create CV object from training data
 diabetes_cv <- vfold_cv(diabetes_train)
 
 # define the recipe, we can always add more recipes
 diabetes_recipe <- 
-  recipe(diabetes ~ age + wt + gh + tri + bmi, 
-         data = table_clean) %>%
+  recipe(Affected ~ Height + Weight + BMI + Dur_disease + genderBin + Above15
+         + Betw5_11 + Below5, 
+         data = lr_fit) %>%
   # and some pre-processing steps
   step_normalize(all_numeric()) %>%
   step_impute_knn(all_predictors())
@@ -63,8 +78,9 @@ lr_workflow <- workflow() %>%
   # add the model
   add_model(lr_model)
 
-# specify which values eant to try
+# specify which values meant to try
 lr_grid <- expand.grid(mtry = c(3, 4, 5))
+
 # extract results
 lr_tune_results <- lr_workflow %>%
   tune_grid(resamples = diabetes_cv, #CV object
@@ -92,10 +108,11 @@ test_performance
 test_predictions <- lr_fit %>% collect_predictions()
 test_predictions
 
-final_model <- fit(lr_workflow, table_clean)
+final_model <- fit(lr_workflow, lr_fit)
 
-new_example <- tribble(~age, ~wt, ~gh, ~tri, ~bmi,
-                       60.16667, 116.8, 6.0, 29.6, 42.39)
+new_example <- tribble~(~Height, ~Weight, ~BMI, ~Dur_disease, ~genderBin, ~Above15,
+                       ~Betw5_11, ~Below5,
+                       120, 52, 21, 1000, 1, 1, 0, 0)
 new_example
 
 predict(final_model, new_data = new_example)
