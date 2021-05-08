dba79ecab79d17b2b6aabb3b36d36dc1d8287dc3
HCEhlers <holgerehlers@hotmail.com>
2021-05-08 13:08:01 +0000

    logistic regression does not provide significant parameters

--- R/07_lr_classification.R
+++ R/07_lr_classification.R
@@ -19,19 +19,21 @@ my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
 lr_fit_data <- my_data_clean_aug %>% 
   mutate(Affected_factor = case_when(Affected == "No" ~ 0,
                               Affected == "yes" ~ 1)) 
-lr_fit_data <- lr_fit_data %>% mutate_if(is.double, as.factor) %>% 
-  mutate_if(is.character, as.factor)
+lr_fit_data <- lr_fit_data %>% mutate_if(is.double, as.numeric) %>% select(where(is.numeric))
 
-lr_fit_data
+View(lr_fit_data)
 
 
 glm.fit <- glm(Affected_factor ~ Height + Weight + BMI + Dur_disease + genderBin +
-                 Above15 + Betw5_11 + Betw11_15,
+                 Above15 + Betw5_11 + Betw11_15 + Below5 + AdeqNutrBin + EducMotherBin +
+                 AutoAB_bin + ImpairedGMBin + InsulinTakenBin + FamHistT1DBin + FamHistT2DBin +
+                 HypoglycemisBin,
                data = lr_fit_data,
                family = binomial)
 
 summary(glm.fit)
 
+# No significant parameters ...
 glm.probs <- predict(glm.fit,type = "response")
 glm.probs
 
