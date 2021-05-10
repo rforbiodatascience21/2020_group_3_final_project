f46c16c9134310cae5264b71013111140fb7dd53
kiara_szakter <szakterkiara@gmail.com>
2021-05-10 06:43:16 +0000

    edit in the presentation

--- R/01_load.R
+++ R/01_load.R
@@ -11,3 +11,4 @@ my_data <- read_csv2(file = "/cloud/project/data/_raw/untidy_t1_diabetes.csv")
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data,
           file = "data/01_my_data_load.tsv")
+
