609a0655f172b2e9570110cb4a86d824134e4535
HCEhlers <holgerehlers@hotmail.com>
2021-05-10 16:51:16 +0000

    last changes

--- R/00_doit.R
+++ R/00_doit.R
@@ -2,7 +2,7 @@
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
-source(file = "R/04_DataVisualization.R") # look at explorative analysis and maybe add histogram
+source(file = "R/04_DataVisualization.R")
 source(file = "R/05_PCA.R")
 source(file = "R/06_lr_model.R")
 rmarkdown::render(input = "doc/Final_Project.Rmd",
